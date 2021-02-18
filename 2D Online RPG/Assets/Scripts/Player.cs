b6f2eff11b25b2eea0eab18a6ff228a4fcf1f7e7
Dennys Marcos <kardeenx@gmail.com>
2021-02-18 19:44:38 -0300

    Basic network setup and Basic Tilemap

---
+++ 2D Online RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    
+    void HandleMovement()
+    {
+        if(isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector2(moveHorizontal * 0.1f, moveVertical * 0.1f);
+            transform.position = transform.position + movement;
+            
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+
+    }
+}
---
+++ 2D Online RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Mirror.Experimental;
+
+[RequireComponent(typeof(NetworkRigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : NetworkBehaviour
+{
+    float input_x = 0;
+    float input_y = 0;
+    bool isWalking = false;
+    float speed = 5.0f;
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        if(isLocalPlayer)
+        {
+            isWalking = false;
+
+            // Get and store a reference to the Rigidbody2D component so we can access it.
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+        
+    }
+
+    void HandleMovement()
+    {
+        if(isLocalPlayer)
+        {
+            input_x = Input.GetAxisRaw("Horizontal");
+            input_y = Input.GetAxisRaw("Vertical");
+            isWalking = (input_x != 0 || input_y != 0);
+
+            Vector2 movement = new Vector2(input_x * 0.4f, input_y * 0.4f);
+
+            //transform.position = transform.position + movement;
+            rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
+            
+        }
+    }
+}
